c0e4b2cdaaf93badf57a33277a0dfea1ffe48807
benjaminhyw <benjaminhyw@gmail.com>
2020-05-12 14:41:09 -0700

    Create variables for WEATHERAPIKEY & BASEURL

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+const WEATHERAPIKEY = "d14498e1d6441199d14053bf26d766d9";
+const BASEURL = "api.openweathermap.org/data/2.5/weather";
 
 // Create a new date instance dynamically with JS
 let d = new Date();

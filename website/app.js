c0975291a878f1fcd46292b9fb4797f6e9cdbe32
benjaminhyw <benjaminhyw@gmail.com>
2020-05-12 16:14:31 -0700

    Successfully call api with zipcode when user clicks button

--- website/app.js
+++ website/app.js
@@ -1,7 +1,21 @@
 /* Global Variables */
 const WEATHERAPIKEY = "d14498e1d6441199d14053bf26d766d9";
-const BASEURL = "api.openweathermap.org/data/2.5/weather";
+const BASEURL = "https://api.openweathermap.org/data/2.5/weather?";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+const generateButton = document.getElementById("generate");
+generateButton.addEventListener("click", async (event) => {
+  const zipInput = document.getElementById("zip");
+  event.preventDefault();
+  const weatherData = await fetchWeatherData(zipInput.value);
+
+  console.log(weatherData);
+});
+
+async function fetchWeatherData(zipCode) {
+  const query = `${BASEURL}zip=${zipCode}&appid=${WEATHERAPIKEY}`;
+  return fetch(query);
+}
